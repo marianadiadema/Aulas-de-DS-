67b64d28e239ac939bb4a6aab8126e147b6cb68b
Mariana(Diadema) <marianareisdomingues@gmail.com>
2024-04-26 09:36:25 -0300

    Create PessoaJuridica.java

---
+++ PessoaJuridica.java
@@ -0,0 +1,10 @@
+public class PessoaJuridica extends Pessoa{
+    private String cnpj;
+    public void setCnpj(String cnpj){
+        this.cnpj;
+    }
+    public String getCnpj(){
+        return(cnpj);
+    }
+}
+ 
