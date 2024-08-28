fde64db8fe80603677bebe79d52d49e97f699bdb
Mariana Reis Domingues <marianareisdomingues@gmail.com>
2024-08-28 13:39:49 -0300

    Update ContadorWhile.java

--- Projetos-de-Java-main/Conjunto-de-Java-main/TopMelhoresProjetosdoMundo/src/ContadorWhile.java
+++ Projetos-de-Java-main/Conjunto-de-Java-main/TopMelhoresProjetosdoMundo/src/ContadorWhile.java
@@ -6,17 +6,17 @@ public class ContadorWhile { //abre a classe pública ContadorWhile
             int limite = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade!: ")); //cria uma variável que pede informações para o usuário
             int contador = limite; //abre uma variável que pega a resposta da variável anterior
             while (contador >= 0) { //abre outro método de repetição, esse sendo o while
-                System.out.println(contador);
-                contador--;
+                System.out.println(contador); // coloca na tela a informações
+                contador--; // Faz a conta
             }
-            System.out.println("Fim do contador regressivo\n");
-            contador = 0;
-            do {
+            System.out.println("Fim do contador regressivo\n"); // coloca na tela a finalização 
+            contador = 0; // Finaliza a conta 
+            do { // Vai fazer que o while começar e depois checar isso 
                 System.out.println(contador);
                 contador++;
-            } while (contador <= limite);
+            } while (contador <= limite); // é um looping quando você não ter um "fim" definido 
             System.out.println(("Fim do contador progressivo."));
-        } catch (NumberFormatException erro) {
+        } catch (NumberFormatException erro) { // Se tiver um erro, ele vai achar e mostra-la
             System.out.println("Não foi encontrado um número inteiro válido!\n" + erro.toString()); // se o argumento for inválido
         }
         System.exit(0);
